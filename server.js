c7204cb54639115417f151ffbf809a050d3a1e4b
bijay shaw <bijayshaw1999@outlook.com>
2023-10-28 15:44:49 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from "express"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.get("/",(req, res) => {
+    return res.send({ success: true });
+})
+
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`);
+  });
